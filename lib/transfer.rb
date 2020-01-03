0490a652053d336a2dfc9e34c4d8df8a4c85d444
kelly r maass <kmaass77627@gmail.com>
2020-01-03 21:15:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,42 @@
 class Transfer
-  # your code here
+  
+  
+  attr_accessor :sender, :receiver, :amount, :status 
+  
+  def initialize(sender, receiver, amount) 
+    @sender = sender
+    @receiver = receiver
+    @amount = amount 
+    @status = 'pending' 
+  end 
+  
+  
+  def valid?
+    @sender.valid? && @receiver.valid? ? true : false
+  end 
+  
+  def execute_transaction
+    if self.status == "pending" && self.valid?
+      self.sender.balance -= @amount
+      self.receiver.balance += @amount
+      self.status = "complete"
+    end
+  end 
+  
+  
+  
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
+    
+    
+  end 
+    
+  
+  
+  
+  
 end
